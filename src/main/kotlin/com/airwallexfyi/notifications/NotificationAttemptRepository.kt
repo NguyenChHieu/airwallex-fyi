c06d92bf793eb08f322f934c29b7af517f8c29a2
NguyenChHieu <nguyenchihieu178205@gmail.com>
2026-06-21 11:34:40 +1000

    refactor: remove unused generated scaffolding

--- src/main/kotlin/com/airwallexfyi/notifications/NotificationAttemptRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.airwallexfyi.notifications
-
-import java.util.UUID
-import org.springframework.data.repository.CrudRepository
-
-interface NotificationAttemptRepository : CrudRepository<NotificationAttemptRecord, UUID> {
-    fun findByPostId(postId: UUID): List<NotificationAttemptRecord>
-}
--- src/main/kotlin/com/airwallexfyi/summaries/SummaryRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.airwallexfyi.summaries
-
-import java.util.UUID
-import org.springframework.data.repository.CrudRepository
-
-interface SummaryRepository : CrudRepository<SummaryRecord, UUID> {
-    fun findByPostId(postId: UUID): SummaryRecord?
-}
